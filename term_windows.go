a501edd6dd5173cca449b16047be3400af4b5626
Jordan Orelli <jordanorelli@gmail.com>
2016-12-07 15:25:45 -0800

    support windows

---
+++ term_windows.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+var kernel32 = syscall.NewLazyDLL("kernel32.dll")
+var procGetConsoleMode = kernel32.NewProc("GetConsoleMode")
+
+// IsTerminal returns true if the given file descriptor is a terminal.
+func IsTerminal(fd uintptr) bool {
+	var st uint32
+	r, _, e := syscall.Syscall(procGetConsoleMode.Addr(), 2, uintptr(fd), uintptr(unsafe.Pointer(&st)), 0)
+	return r != 0 && e == 0
+}
